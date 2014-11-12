73541f735b2b7349ecd1a6cfc84eaff34fa22608
Andrew Landry <andrewlandry@gmail.com>
2014-11-12 18:02:45 -0500

    setup bare animatedGif class

--- src/animatedGif.cpp
+++ src/animatedGif.cpp
@@ -7,3 +7,21 @@
 //
 
 #include "animatedGif.h"
+
+animatedGif::animatedGif()
+{
+    x = ofRandom(0, ofGetWidth());      // give some random positioning
+    y = ofRandom(0, ofGetHeight());
+    
+    speedX = ofRandom(-1, 1);           // and random speed and direction
+    speedY = ofRandom(-1, 1);
+    
+}
+
+void animatedGif::update() {
+
+}
+
+void animatedGif::draw() {
+
+}
