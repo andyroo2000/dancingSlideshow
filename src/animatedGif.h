d31396badc6d6bd484720f7a6856606805dce4d5
Andrew Landry <andrewlandry@gmail.com>
2014-11-13 11:30:02 -0500

    clean up formatting

--- src/animatedGif.h
+++ src/animatedGif.h
@@ -9,26 +9,23 @@
 
 class AnimatedGif {
     
-public: // place public functions or variables declarations here
-    
+public:
     // methods, equivalent to specific functions of your class objects
     void setup();
-    void update();  // update method, used to refresh your objects properties
-    void draw();    // draw method, this where you'll do the object's drawing
+    void update();
+    void draw();
+    void setValueX(int _x);
     
+    // variables
     ofImage myImage;  // variable in object to load each spritesheet
-    
-    int xPositionOfSprite = 0;
-    
     float x = 0;  // parameter - x-position of gif
     float y = 0;  // parameter - y-position of gif
+    int xPositionOfSprite = 0;
     int incrementAmount = 600;  // size of offset for each image in spritesheet
     int numberOfFrames = 6;  // number of frames in the gif
     int savedTime = 0;
-    int totalTime = 400;  // time for each frame of gif in milliseconds
+    int totalTime = 200;  // time for each frame of gif in milliseconds
     int currentTime = 0;
-    
-    void setValueX(int _x);
     
     // constructor
     AnimatedGif(string _imagePath, float _x, float _y);
